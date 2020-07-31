91266d4d99c64e79731c230b25cef7eeba7c4433
harika penukonda <penukondaharika@gmail.com>
2020-07-31 17:08:05 -0500

    Access and Display method signature - JoinPoints

--- src/com/udemy/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/udemy/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,10 @@
 package com.udemy.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +15,15 @@ public class MyDemoLoggingAspect {
 	
 	
 	@Before("com.udemy.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()") // Apply pointcut declaration to advice
-	public void beforeAddAccountAdvice()
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) // JoinPoint has the metadata about method call
 	{
 		System.out.println("\n===> Performing beforeAddAccountAdvice ");
+		
+		// display the method signature
+		MethodSignature methodSign = (MethodSignature)theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSign);
+		// display the method arguments
+		
 	}
 	
 	
